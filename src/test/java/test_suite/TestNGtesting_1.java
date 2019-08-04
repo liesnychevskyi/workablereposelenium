afea43406925f008e3f0492e4ac29829cf0f9e93
liesnychevskyi <liesnychevskyi.s@gmail.com>
2019-08-04 16:59:39 +0300

    Added new helpers : refresh just

---
+++ src/test/java/test_suite/TestNGtesting_1.java
@@ -0,0 +1,23 @@
+package test_suite;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGtesting_1
+{
+    int i = 1;
+    @Test
+    public void testLogin()
+    {
+        if(i == 3)
+        {
+            Assert.assertTrue(true);
+        }
+        else
+        {
+            System.out.println("it is = " + i);
+            i++;
+            Assert.assertTrue(false);
+        }
+    }
+}
